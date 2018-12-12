e759867116ecc2104e1dcdd6f9ea658b2ad610e1
maliang <maliang@xywy.com>
2018-12-12 12:59:41 +0800

    ml

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -21,7 +21,9 @@ class UserController extends Controller
             $data = ApiRequest::login($code);
             if($data){
                 $this->data['msg'] = self::API_CODE_SUCCESS_MSG;
-                $this->data['sign'] =  md5($data['openid'] . rand(10000,99999) . $data['session_key']);
+                $this->data['data'] =  [
+                    'sign' => md5($data['openid'] . rand(10000,99999) . $data['session_key'])
+                ];
                 Yii::$app->cache->set($this->data['sign'], $data, 1800);
             }else{
                 $this->data['msg'] = '服务器异常';
