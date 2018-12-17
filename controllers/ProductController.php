ecd2e9909a18a6b29fd82f963af5443bde026812
maliang <maliang@xywy.com>
2018-12-17 09:49:14 +0800

    ml

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -31,27 +31,81 @@ class ProductController extends Controller
             $this->data['allPages'] = 3;
             $this->data['data'] = [
                 [
+                    'id' => 1,
                     'title' => 'LANCOME兰蔻小黑瓶精华肌底液',
+                    'price' => 188.00,
+                    'memberPrice' => 168.00,
+                    'memberName' => '银卡专享',
+                    'labelArray' => [
+                        '纯天然',
+                        '价格实惠',
+                        '优质产品'
+                    ],
                     'imgUrl' => 'http://mz.djmall.xmisp.cn/files/product/20161201/148057921620_middle.jpg'
                 ],
                 [
+                    'id' => 2,
                     'title' => 'LANCOME兰蔻小黑瓶精华肌底液-1',
+                    'price' => 188.00,
+                    'memberPrice' => 168.00,
+                    'memberName' => '银卡专享',
+                    'labelArray' => [
+                        '纯天然',
+                        '价格实惠',
+                        '优质产品'
+                    ],
                     'imgUrl' => 'http://mz.djmall.xmisp.cn/files/product/20161201/148057922659_middle.jpg'
                 ],
                 [
+                    'id' => 3,
                     'title' => 'LANCOME兰蔻小黑小黑瓶精华小黑瓶精华瓶精华肌底液-2',
+                    'price' => 188.00,
+                    'memberPrice' => 168.00,
+                    'memberName' => '银卡专享',
+                    'labelArray' => [
+                        '纯天然',
+                        '价格实惠',
+                        '优质产品'
+                    ],
                     'imgUrl' => 'http://mz.djmall.xmisp.cn/files/product/20161201/148057923813_middle.jpg'
                 ],
                 [
+                    'id' => 4,
                     'title' => 'LANCOME兰蔻小黑瓶精华肌底液-3',
+                    'price' => 188.00,
+                    'memberPrice' => 168.00,
+                    'memberName' => '银卡专享',
+                    'labelArray' => [
+                        '纯天然',
+                        '价格实惠',
+                        '优质产品'
+                    ],
                     'imgUrl' => 'http://mz.djmall.xmisp.cn/files/product/20161201/148057924965_middle.jpg'
                 ],
                 [
+                    'id' => 5,
                     'title' => 'LANCOME兰蔻小小黑瓶精华黑瓶精华肌底液-4',
+                    'price' => 188.00,
+                    'memberPrice' => 168.00,
+                    'memberName' => '银卡专享',
+                    'labelArray' => [
+                        '纯天然',
+                        '价格实惠',
+                        '优质产品'
+                    ],
                     'imgUrl' => 'http://mz.djmall.xmisp.cn/files/product/20161201/148057925958_middle.jpg'
                 ],
                 [
+                    'id' => 6,
                     'title' => 'LANCOME兰蔻小黑瓶精华肌底液-5',
+                    'price' => 188.00,
+                    'memberPrice' => 168.00,
+                    'memberName' => '银卡专享',
+                    'labelArray' => [
+                        '纯天然',
+                        '价格实惠',
+                        '优质产品'
+                    ],
                     'imgUrl' => 'http://mz.djmall.xmisp.cn/files/product/20161201/148057923813_middle.jpg'
                 ]
             ];
@@ -65,6 +119,8 @@ class ProductController extends Controller
      */
     public function actionDetail()
     {
+        $requestData = Yii::$app->request->get();
+        $idArray = (int) ArrayHelper::getValue($requestData, 'id', []);
         $this->data['code'] = self::API_CODE_SUCCESS;
         $this->data['data'] = [
             'title' => 'LANCOME兰蔻小黑瓶精华肌底液',
