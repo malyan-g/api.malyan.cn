db4ae26847d75551da6193307de93b4e3e62f624
maliang <maliang@xywy.com>
2018-12-31 17:01:51 +0800

    ml

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -268,7 +268,9 @@ class UserController extends Controller
                         if(isset($result['key'])){
                             $model = UserImage::findOne(['user_id' => $this->userId]);
                             if($model){
-                                QiniuApiHelper::delete($model->certificate_url);
+                                if($model->certificate_url){
+                                    QiniuApiHelper::delete($model->certificate_url);
+                                }
                             }else{
                                 $model = new UserImage();
                                 $model->user_id = $this->userId;
@@ -335,7 +337,9 @@ class UserController extends Controller
                         if(isset($result['key'])){
                             $model = UserImage::findOne(['user_id' => $this->userId]);
                             if($model){
-                                QiniuApiHelper::delete($model->contract_url);
+                                if($model->contract_url){
+                                    QiniuApiHelper::delete($model->contract_url);
+                                }
                             }else{
                                 $model = new UserImage();
                                 $model->user_id = $this->userId;
