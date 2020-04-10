73e1ee5ce7b69e5bbe339ca54fe97be3b7d1879c
maliang <maliang@xywy.com>
2020-04-10 17:43:39 +0800

    ml

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -28,7 +28,8 @@ class Controller extends \yii\web\Controller
     public $userId;
     public $permissions = [
         'index/index',
-        'user/login'
+        'user/login',
+        'gii'
     ];
 
     /**
