d08474970ff0261821e822dc2a9f6cb2f3a18a62
malyan <git@malyan.cn>
2020-04-11 19:26:07 +0800

    'ml'

--- config/const.php
+++ config/const.php
@@ -9,7 +9,7 @@
 define('PASSPORT_URL', 'http://passport.malyan.cn');
 define('BLOG_URL', 'http://blog.malyan.cn');
 define('TOKEN_EXPIRE', 1800);
-define('LOGIN_EXPIRE', 1800);
+define('LOGIN_EXPIRE', 30);
 define('API_KEY', '12345678');
 define('SC_SECRET_KEY', 'VZXptdtHFs/k+I2Cfrj1+5kuO7r3ppAI');
 define('API_CODE_ERROR', 0);
