7566e8a99534dc3c4986e6f1622452879830ad08
maliang <maliang@xywy.com>
2020-04-13 15:26:51 +0800

    ml

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -25,35 +25,42 @@ class ImageController extends Controller
     {
         $data = [
             [
+                'id' => 1,
                 'title' => '云南大理游',
                 'url' => 'http://img.malyan.cn/a2c7d9d5b897b43ba08bb71f.jpg',
                 'count' => 123
             ],
             [
+                'id' => 2,
                 'title' => '云南丽江游',
                 'url' => 'http://img.malyan.cn/693470e935aa1ecfa2f90b80810042e5.jpeg',
                 'count' => 4
             ],
             [
+                'id' => 3,
                 'title' => '云南天水游',
                 'url' => 'http://img.malyan.cn/30850e61a1256de58cb10d19.jpg',
                 'count' => 542
             ],[
+                'id' => 4,
                 'title' => '云南天水游',
                 'url' => 'http://img.malyan.cn/a2c7d9d5b897b43ba08bb71f.jpg',
                 'count' => 109
             ],
             [
+                'id' => 5,
                 'title' => '云南天水游',
                 'url' => 'http://img.malyan.cn/7acb0a46f21fbe09d6bb8c6065600c338644add9.jpg',
                 'count' => 2334
             ],
             [
+                'id' => 6,
                 'title' => '云南天水游',
                 'url' => 'http://img.malyan.cn/71b77de83901213f0ec739d95ae736d12e2e956c.jpg',
                 'count' => 24
             ],
             [
+                'id' => 7,
                 'title' => '云南天水游',
                 'url' => 'http://img.malyan.cn/timg.jpeg',
                 'count' => 33
