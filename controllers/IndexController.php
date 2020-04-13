d10702d6f1fb2abd75eb83a7ac7edf793b80fd94
maliang <maliang@xywy.com>
2020-04-13 09:48:17 +0800

    ml

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -24,7 +24,6 @@ class IndexController extends Controller
     public function actionIndex()
     {
         $model = BaseConfig::findOne(1);
-        return $model;
         $this->data = [
             'code' => self::API_CODE_SUCCESS,
             'msg' => self::API_CODE_SUCCESS_MSG,
