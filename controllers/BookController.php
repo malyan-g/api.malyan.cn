0d659cfdaf158326879c4c4bff188805acccbb24
maliang <maliang@xywy.com>
2020-04-15 11:39:40 +0800

    ml

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -99,6 +99,7 @@ class BookController extends Controller
         $id = (int) ArrayHelper::getValue($requestData, 'id', 1);
         if($id > 0){
             $queryData = $first == true ? ['book_id' => $id, 'sort' => 1] : ['id' => $id];
+            return $queryData;
             $catalogData = BookCatalog::find()
                 ->select(['id', 'book_id', 'title'])
                 ->where($queryData)
