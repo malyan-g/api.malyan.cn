bb17a57d0a2985d4bbddad488e5cea83b7e3adc6
maliang <maliang@xywy.com>
2020-04-16 16:55:01 +0800

    ml

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -54,7 +54,7 @@ class BookController extends Controller
                     ->column();
 
                 foreach ($bookData as $key => $val){
-                    $data[$key]['content'] = mb_substr($detailData[$val], 0, 200);
+                    $data[$key]['content'] = mb_substr($detailData[$val], 0, 100);
                 }
 
                 $this->data = [
